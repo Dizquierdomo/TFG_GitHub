63e42d0b3c7b36cbea8ef845c5579d93211ebf87
Dizquierdomo <102058920+Dizquierdomo@users.noreply.github.com>
2022-03-25 00:28:19 +0100

    TEST

--- UnrealEngine_TFG/Source/UnrealEngine_TFG/UnrealEngine_TFG.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UnrealEngine_TFG.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealEngine_TFG, "UnrealEngine_TFG" );
--- UnrealEngine_TFG/Source/UnrealEngine_TFG/UnrealEngine_TFGGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "UnrealEngine_TFGGameModeBase.h"
-
